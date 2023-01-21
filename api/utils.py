7f3812032cc172905a79ccfb1e607a5a561f3504
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-01-21 16:29:07 -0500

    add: endpoint to batch add restaurants

--- api/utils.py
+++ api/utils.py
@@ -26,3 +26,39 @@ def addRestaurant():
     except KeyError:
         return jsonify({'result': 'invalid restaurant format'}), 400
 
+
+
+@utils.route('/utils/addBatchRestaurantsRaw', methods = ['POST'])
+def addBatchRestaurantsRaw():
+    data = request.json
+    data = data['restaurants']
+
+    for i in range(len(data)):
+        try:
+            desc = data[i].get('editorial_summary')
+            if desc is not None:
+                desc = desc.get('overview', '')
+            else:
+                desc = ''
+            if data[i].get('photos', []) != []:
+                photo = data[i].get('photos')[0]
+                photo = photo.get('photo_reference')
+            else:
+                photo = ''
+
+            db_manager.addRestaurant(
+                data[i].get('formatted_address', ''),
+                data[i].get('category', ''),
+                desc,
+                data[i].get('name', ''),
+                data[i].get('formatted_phone_number', ''),
+                photo,
+                data[i].get('place_id', ''),
+                data[i].get('price_level', ''),
+                data[i].get('rating'),
+                [review.get('text', '') for review in data[i].get('reviews', [])],
+                data[i].get('website', '')
+            )
+        except KeyError:
+            pass
+    return jsonify({'result': 'success'}), 200
