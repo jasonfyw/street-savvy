31d485c839ca1c96376a7e139d10be6969a766d5
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-01-22 01:49:08 -0500

    refactor: navbar with chakra

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef } from 'react';
+import {
+    Box,
+    Flex,
+    HStack,
+    IconButton,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    Text,
+    Link
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { Link as RouteLink } from 'react-router-dom';
+
+
+const NavLink = ({ children, to }) => (
+    <Link as={RouteLink} to={to} p={2} fontWeight={300} borderRadius={10} _hover={{
+        textDecoration: 'none',
+        bg: '#CE7A67',
+        transition: '0.2s',
+        cursor: 'pointer'
+    }}>
+        { children }
+    </Link>
+);
+
+
+const Nav= () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const ref = useRef(null);
+
+    const links = [
+        <NavLink to={'/about'}>About</NavLink>,
+        <NavLink to={'/findaplace'}>Discover a new place</NavLink>
+
+    ]
+
+    // lifecycle hook to close mobile navbar when there is a mousedown event
+    // outside of the navbar
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) onClose()
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [onClose, ref]);
+
+    return (
+        <>
+            <Box
+                bg={useColorModeValue('#DE8A77', '#DE8A77')}
+                px={4}
+                position={'fixed'}
+                top={0}
+                width={'100vw'}
+                zIndex={100}
+                ref={ref}
+            >
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <Flex alignItems={'center'} pl={4} >
+                        <Link
+                            as={RouteLink}
+                            to={'/'}
+                            _hover={{
+                                textDecoration: 'none',
+                                transition: '0.8s',
+                                cursor: 'pointer'
+                            }}
+                        >
+                            {/* <Box fontWeight={600}>Jason Wang</Box> */}
+                            <Text fontWeight={600} fontSize={30} fontFamily={'Calligraffitti, cursive;'}>Street Savvy</Text>
+                        </Link>
+                    </Flex>
+                    <HStack spacing={8} alignItems={'center'} mr={6}>
+                        <HStack as={'nav'} spacing={6} display={{ base: 'none', md: 'flex' }}>
+                            { links }
+                            <Link p={2} fontWeight={300} borderRadius={10} _hover={{
+                                textDecoration: 'none',
+                                bg: '#CE7A67',
+                                transition: '0.2s',
+                                cursor: 'pointer'
+                            }}>
+                                Login
+                            </Link>
+                        </HStack>
+                    </HStack>
+                </Flex>
+
+                {isOpen ? (
+                    <Box pb={4} display={{ md: 'none' }}>
+                        <Stack as={'nav'} spacing={4}>
+                            { links }
+                        </Stack>
+                    </Box>
+                ) : null}
+            </Box>
+        </>
+    );
+}
+
+export default Nav;
