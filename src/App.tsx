47e534f55d66993650ba41099f2df00447d53fd1
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-01-26 01:23:01 -0500

    add: files from js version

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,46 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Explore from './components/Explore';
+import Login from './components/Login';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Route, Router } from 'react-router-dom';
+import axios from 'axios'
+import List from './components/List'
+import { User } from 'firebase/auth'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const auth = getAuth();
+
+const App = () => {
+    const [user, setUser] = useState<User|undefined>(undefined)
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            // @ts-ignore
+            setUser(user);
+            // @ts-ignore
+            axios.post('http://127.0.0.1:5000/userreg/'.concat(user['uid']), {
+                // @ts-ignore
+                'email': user['email']
+            })
+        })
+    }, [])
+
+    return (
+        <ChakraProvider>
+            {/* <Router>
+                <Route to={'/'}> */}
+            {
+                user ? (
+                    <Explore user={user} />
+                ) : (
+                    <Login />
+                )
+            }
+            {/* </Route>
+                <Route to={'/lists'}><List /></Route> */}
+            {/* </Router> */}
+        </ChakraProvider>
+    )
+}
+
+export default App;
