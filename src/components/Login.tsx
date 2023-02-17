761cf7215c31b7fd32cad1047e10ebeb6a49a412
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-02-16 21:34:21 -0500

    add: improved responsiveness and polygons

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,55 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
 import { provider } from "../firebase";
-import { Box, Center, Stack, Heading, IconButton, VStack, Text, Image } from "@chakra-ui/react";
+import {
+    Box,
+    Center,
+    Stack,
+    Heading,
+    IconButton,
+    VStack,
+    Text,
+    Image,
+    useMediaQuery
+} from "@chakra-ui/react";
 import { FaGoogle } from 'react-icons/fa';
 import img from '../media/house.png'
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
 
 const auth = getAuth();
 
 
 const Login = () => {
+    const [isLargerThan800] = useMediaQuery('(min-width: 800px)')
+
     return (
         <Box>
-            <Stack h={'100vh'} direction={['column', 'row']}>
-                <Box h={['50vh', '100vh']} w={['100vw', '50vw']} bg={'#DE8A77'}>
-                    <Heading fontSize={'100'} color={'#fff'} fontFamily={'Calligraffitti'} fontWeight={400} pt={20} textAlign={'center'}>Street Savvy</Heading>
-                    <Center h={'65%'} mx={'6rem'} pb={50} color={'white'} fontFamily={'Rubik'}>
+            <ColorModeSwitcher
+                right={0}
+                position={'fixed'}
+                m={5} />
+
+            <Stack 
+                h={'100vh'} 
+                direction={isLargerThan800 ? 'row' : 'column'}
+            >
+                <Box 
+                    w={isLargerThan800 ? '55vw' : '100vw'} 
+                    h={isLargerThan800 ? '100vh' : 'auto'} 
+                    bg={'#DE8A77'} 
+                    clipPath={isLargerThan800 ? 'polygon(90% 0, 100% 50%, 90% 100%, 0 100%, 0 0)' : 'polygon(0 0, 100% 0, 100% 90%, 50% 100%, 0 90%)'}
+                >
+                    <Heading
+                        fontSize={'100'}
+                        color={'#fff'}
+                        fontFamily={'Calligraffitti'}
+                        fontWeight={400}
+                        pt={10}
+                        pb={5}
+                        textAlign={'center'}
+                    >
+                        Street Savvy
+                    </Heading>
+                    <Center h={'65%'} mx={isLargerThan800 ? '6rem' : 4} pb={50} color={'white'} fontFamily={'Rubik'}>
                         <VStack>
                             <Text textAlign={'center'} fontSize={20} my={10}>
                                 Street Savvy gives you recommendations based on your budget and interests, whether it be for food, shopping or entertainment. Now you will never have trouble picking a spot or worry about going over your budget!
@@ -22,10 +58,10 @@ const Login = () => {
                         </VStack>
                     </Center>
                 </Box>
-                <Box h={['50vh', '100vh']} w={['100vw', '50vw']} >
+                <Box w={isLargerThan800 ? '45vw' : '100vw'} h={ isLargerThan800 ? '100vh' : 'auto'} >
                     <Center h={'100%'}>
-                        <VStack>
-                            <Heading fontSize={'60'} color={'#DE8A77'} fontFamily={'Rubik, sans-serif'} mb={10}>Get Started</Heading>
+                        <VStack py={10}>
+                            <Heading fontSize={'60'} color={'#DE8A77'} fontFamily={'Rubik, sans-serif'} mb={10} textAlign={'center'}>Get Started</Heading>
                             <IconButton
                                 onClick={() => {
                                     signInWithPopup(auth, provider)
